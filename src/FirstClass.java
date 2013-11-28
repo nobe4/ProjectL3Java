f6ebef9d712f6e3499dc889a6fb75301317f324f
Padawan <haffreinguev@gmail.com>
2013-11-28 15:29:21 +0100

    Frist Commit

---
+++ src/FirstClass.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: padawan
+ * Date: 11/28/13
+ * Time: 3:28 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("MMM MONSTER KILL");
+    }
+}

5660f547d8f33a30e42222e014b869de8b504b86
Padawan <haffreinguev@gmail.com>
2013-11-30 20:31:04 +0100

    Multiple Input Keyboard

--- src/Test.java
+++ src/Test.java
@@ -3,15 +3,13 @@ import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class Test{
+public class Test implements KeyListener {
 
-   private class EventDispatcher implements KeyEventDispatcher{
-       @Override
-       public boolean dispatchKeyEvent(KeyEvent e) {
-           System.out.println(e.toString());
-           return false;
-       }
-   }
+    private enum DIRECTION{UP, DOWN, LEFT, RIGHT;
+    }
+
+    private boolean[] keyPressed = {false,false,false,false};
+    private JPanel block;
 
 
     private static void createAndShowGUI() {
@@ -21,15 +19,13 @@ public class Test{
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 600);
         frame.setVisible(true);
+
         // JFrame is splitted into multiple panes : each panes is like a slice of an onion
         // JPanel is a slice
 
         Test t = new Test();
 
         frame.setContentPane(t.createPanel());
-
-
-
     }
 
     JPanel createPanel(){
@@ -37,19 +33,35 @@ public class Test{
         root.setOpaque(true);
         root.setLayout(null); // disable all layout modification
 
-        JPanel p = new JPanel();
-        p.setSize(100, 100);
-        p.setLocation(100, 100);
-        p.setBackground(Color.black);
-        p.setOpaque(true);
+        block = new JPanel();
+        block.setSize(100, 100);
+        block.setLocation(100, 100);
+        block.setBackground(Color.black);
+        block.setOpaque(true);
 
-        root.add(p); // adding p in the root
+        root.add(block); // adding block in the root
 
-        KeyboardFocusManager manager = KeyboardFocusManager.getCurrentKeyboardFocusManager();
-        manager.addKeyEventDispatcher(new EventDispatcher());
+        root.setFocusable(true);
+        root.addKeyListener(this);
 
         return root;
     }
+
+    void moveBlock (){
+        if (keyPressed[0]) {     // UP
+            block.setLocation(block.getX(), block.getY()-1);
+        }
+        if (keyPressed[1]) {     // DOWN
+            block.setLocation(block.getX(), block.getY()+1);
+        }
+        if (keyPressed[2]) {     // LEFT
+            block.setLocation(block.getX()-1, block.getY());
+        }
+        if (keyPressed[3]) {     // RIGHT
+            block.setLocation(block.getX()+1, block.getY());
+        }
+    }
+
 
     public static void main(String[] args) {
         //Schedule a job for the event-dispatching thread:
@@ -59,5 +71,54 @@ public class Test{
                 createAndShowGUI();
             }
         });
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        //System.out.println(e.toString());
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        //System.out.println(e.toString());
+        switch (e.getKeyCode()){
+            case KeyEvent.VK_UP :
+                keyPressed[0] = true;
+                break;
+            case KeyEvent.VK_DOWN :
+                keyPressed[1] = true;
+                break;
+            case KeyEvent.VK_LEFT :
+                keyPressed[2] = true;
+                break;
+            case KeyEvent.VK_RIGHT :
+                keyPressed[3] = true;
+                break;
+            default:
+                System.out.println("other key printed");
+                break;
+        }
+        moveBlock();
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        switch (e.getKeyCode()){
+            case KeyEvent.VK_UP :
+                keyPressed[0] = false;
+                break;
+            case KeyEvent.VK_DOWN :
+                keyPressed[1] = false;
+                break;
+            case KeyEvent.VK_LEFT :
+                keyPressed[2] = false;
+                break;
+            case KeyEvent.VK_RIGHT :
+                keyPressed[3] = false;
+                break;
+            default:
+                System.out.println("other key printed");
+                break;
+        }
     }
 }
